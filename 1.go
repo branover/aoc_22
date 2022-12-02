a5538b4012f2653c3a0feb92e121f0eb0e5b4dd0
Brandon Overall <branover@gmail.com>
2022-12-02 17:20:26 -0500

    Optimized day 1

--- 1.go
+++ 1.go
@@ -2,51 +2,47 @@ package main
 
 import (
 	"strconv"
-	"sort"
+	// "sort"
 )
 
 func day1a(lines []string) int {
 	var solution int	
-	var elves []int
 	var elf int
 	for _, line := range lines {
 		cals, err := strconv.Atoi(line)
 		if err != nil {
-			elves = append(elves, elf)
 			elf = 0
 			continue
 		}
 		elf += cals
-		
-	}
-
-	for _, elf := range elves {
 		if elf > solution {
 			solution = elf
 		}
 	}
-
 
 	return solution
 }
 
 func day1b(lines []string) int {
 	var solution int	
-	var elves []int
+	var top3 [3]int
 	var elf int
 	for _, line := range lines {
 		cals, err := strconv.Atoi(line)
 		if err != nil {
-			elves = append(elves, elf)
 			elf = 0
 			continue
 		}
 		elf += cals
-		
+
+		for i, top := range top3 {
+			if elf > top {
+				top3[i] = elf
+				break
+			}
+		}
 	}
 
-	sort.Slice(elves, func(i, j int) bool { return elves[i] < elves[j] })
-	top3 := elves[len(elves)-3:]
 	for _, elf := range top3 {
 		solution += elf
 	}
